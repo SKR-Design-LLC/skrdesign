4ebc56db4327dcc045b5ab7381ad3394e8a88b25
Sarah Russell <sarah@skrdesign.com>
2019-08-07 17:05:09 -0600

    Adding base site
    Adding the entire site already built out

---
+++ src/assets/js/app.js
@@ -0,0 +1,27 @@
+import $ from 'jquery';
+import 'what-input';
+
+// Foundation JS relies on a global varaible. In ES6, all imports are hoisted
+// to the top of the file so if we used`import` to import Foundation,
+// it would execute earlier than we have assigned the global variable.
+// This is why we have to use CommonJS require() here since it doesn't
+// have the hoisting behavior.
+window.jQuery = $;
+require('foundation-sites');
+
+// If you want to pick and choose which modules to include, comment out the above and uncomment
+// the line below
+//import './lib/foundation-explicit-pieces';
+
+
+$(document).foundation();
+
+
+$(document).ready(function(){
+    // Back to top-button
+    $('.top-button').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+         return false;
+    });
+
+});
